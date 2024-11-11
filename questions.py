3a184904c0de5e7a4a3ba5803c8a837699b69810
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2024-11-11 22:02:13 +0300

    assignment

--- questions.py
+++ questions.py
@@ -1,55 +1,40 @@
-# Qn 1. Within a class called Cohort class, add a constructer in the Cohort class.
-class Cohort_class:
-   name = 'Musiimenta'
-   description = 'Cohort4'
-   start_date = '20th August'
-print(Cohort_class)
 
+#create a class, a class name starts with a capital letter and always in singular
+# class Cohort:
+#name
+#description
+#start date
+#end date
+#total students number
+
+
+# Qn 1. Within a class called Cohort class, add a constructer in the Cohort class.
 # Adding a constructor
 class Cohort_class:
-  def __init__(self,name, description, start_date):
+  def __init__(self,name, description, start_date, end_date, number_of_students):
       self.name = name
       self.description = description
       self.start_date = start_date
-
-
-Cohort_class = Cohort_class('Musiimenta', 'Cohort4', '20th August')
-
-print(Cohort_class.name) 
-print(Cohort_class.description)
-print(Cohort_class.start_date)
-        
+      self.end_date = end_date
+      self.number_of_students = number_of_students
 
 
 # Create a function/method to the class that prints the Cohort name, and the total number of students.
 # Adding a method.
+  def print_cohort_class_info(self):
+    print(f"Cohort Name: {self.name}")
+    print(f"Total number of Students: {self.number_of_students}")
 
-class Cohort_class:
-   def __init__(self,name, description, start_date):
-    self.name = name
-    self.description = description
-    self.start_date = start_date
-
-
-   def total(self):
-            print(f'The total number of students is 59  {self.name}')
-Details = Cohort_class('Musiimenta', 'Cohort4', '20th August')
-Details.total()       
+      
 
  # Qn 3. Create a new instance/object of the cohort class.  
 
-# New object
-
-class Cohort_class:
-     def __init__(self,name, description, start_date):
-          self.name = name
-          self.description = description
-          self.start_date = start_date
-     def Cohort_class_func(self):
-      print(self.name, self.description, self.start_date)
-class Subject(Cohort_class):
-     def __init__(self, name, description, start_date ):
-        super(). __init__(name, description, start_date)
-        self.Subject = 'Computer Science' 
-details = Subject('Musiimenta', 'Cohort4', '20th August')
-print(details.Subject)
+# New object/instance
+cohort = Cohort_class(
+   name = 'Musiimenta',
+   description = 'Cohort4',
+   start_date = '20th August',
+   end_date = '26th June',
+   number_of_students = 56
+)
+cohort.print_cohort_class_info()
